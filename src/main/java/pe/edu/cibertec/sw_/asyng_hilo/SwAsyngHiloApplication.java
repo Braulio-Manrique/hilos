2abe8d07e93920f79a8b2af264668d6c0f8f6ab1
Braulio-Manrique <brauliocuenta12@gmail.com>
2024-09-29 09:20:23 -0500

    iniciando proyecto

---
+++ src/main/java/pe/edu/cibertec/sw_/asyng_hilo/SwAsyngHiloApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.sw_.asyng_hilo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwAsyngHiloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwAsyngHiloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/sw_/asyng_hilo/SwAsyngHiloApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.sw_.asyng_hilo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwAsyngHiloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
